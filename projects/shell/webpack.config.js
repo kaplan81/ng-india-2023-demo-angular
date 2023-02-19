0da28ff3591b315f591a3ad78ea775d6bce53fe6
Andres Gesteira <andres.gesteira@henkel.com>
2023-02-19 21:19:54 +0100

    add shell accurate webpack configs

--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -1,56 +1,60 @@
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const mf = require("@angular-architects/module-federation/webpack");
-const path = require("path");
-const share = mf.share;
+// @ts-check
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const { share, SharedMappings } = require('@angular-architects/module-federation/webpack');
+const sharedMappings = new SharedMappings();
 
-const sharedMappings = new mf.SharedMappings();
-sharedMappings.register(
-  path.join(__dirname, '../../tsconfig.json'),
-  [/* mapped paths to share */]);
-
-module.exports = {
+const shellWebpackConfig = {
   output: {
-    uniqueName: "shell",
-    publicPath: "auto"
+    publicPath: 'auto',
   },
   optimization: {
-    runtimeChunk: false
-  },   
-  resolve: {
-    alias: {
-      ...sharedMappings.getAliases(),
-    }
+    runtimeChunk: false,
   },
   experiments: {
-    outputModule: true
+    outputModule: true,
   },
   plugins: [
     new ModuleFederationPlugin({
-        library: { type: "module" },
-
-        // For remotes (please adjust)
-        // name: "shell",
-        // filename: "remoteEntry.js",
-        // exposes: {
-        //     './Component': './projects/shell/src/app/app.component.ts',
-        // },        
-        
-        // For hosts (please adjust)
-        // remotes: {
-        //     "mfe1": "http://localhost:3000/remoteEntry.js",
-
-        // },
-
-        shared: share({
-          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-
-          ...sharedMappings.getDescriptors()
-        })
-        
+      library: { type: 'module' },
+      remotes: {
+        mfes: 'http://localhost:4201/remoteEntry.js',
+      },
+      shared: share({
+        '@angular/core': {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: 'auto',
+          eager: true,
+          includeSecondaries: {
+            skip: ['@angular/core/testing'],
+          },
+        },
+        '@angular/common': {
+          singleton: true,
+          strictVersion: true,
+          requiredVersion: 'auto',
+          eager: true,
+          includeSecondaries: {
+            skip: [
+              '@angular/common/http',
+              '@angular/common/http/testing',
+              '@angular/common/testing',
+              '@angular/common/upgrade',
+            ],
+          },
+        },
+      }),
     }),
-    sharedMappings.getPlugin()
+    sharedMappings.getPlugin(),
   ],
 };
+
+// // Uncomment for debugging.
+// console.log('shellWebpackConfig:::', shellWebpackConfig);
+// console.log(
+//   'ModuleFederationPlugin options:::',
+//   // @ts-ignore
+//   shellWebpackConfig.plugins[0]._options,
+// );
+
+module.exports = shellWebpackConfig;
