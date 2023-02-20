1ca66cf9f067264b0db2b7f12434e589fbd18e11
Andres Gesteira <andres.gesteira@henkel.com>
2023-02-20 17:31:32 +0100

    pass properties to widgets

--- projects/external/widget.js
+++ projects/external/widget.js
@@ -1,23 +1,32 @@
 // @ts-check
 'use-strict';
 
-function setWidget(number) {
-  const exposed = `./mfe${number}`;
-  const hostElelementWrapper = document.getElementById('host');
-  if (hostElelementWrapper !== null) {
-    const hasChildNodes = hostElelementWrapper.hasChildNodes();
-    if (hasChildNodes) {
-      while (hostElelementWrapper.firstChild) {
-        hostElelementWrapper.removeChild(hostElelementWrapper.firstChild);
+function setWidget(id, attrs) {
+  if (attrs.exposed === undefined) {
+    throw Error('exposed is a mandatory attribute');
+  }
+  const ngWidgets = document.getElementsByClassName('ng-widget');
+  if (ngWidgets.length > 0) {
+    for (const ngWidget of ngWidgets) {
+      const hostElelementWrapper = ngWidget.querySelector(`#${id}`);
+      if (hostElelementWrapper !== null) {
+        const hasChildNodes = hostElelementWrapper.hasChildNodes();
+        if (hasChildNodes) {
+          while (hostElelementWrapper.firstChild) {
+            hostElelementWrapper.removeChild(hostElelementWrapper.firstChild);
+          }
+        }
+        try {
+          const element = document.createElement('shl-host');
+          for (const [key, value] of Object.entries(attrs)) {
+            element.setAttribute(key, value);
+          }
+          const fragment = document.createRange().createContextualFragment(element.outerHTML);
+          hostElelementWrapper.appendChild(fragment);
+        } catch (error) {
+          console.error('Error while loading snippet:::', error);
+        }
       }
-    }
-    try {
-      const fragment = document
-        .createRange()
-        .createContextualFragment(`<shl-host exposed="${exposed}"></shl-host>`);
-      hostElelementWrapper.appendChild(fragment);
-    } catch (error) {
-      console.error('Error while loading snippet:::', error);
     }
   }
 }
