f3e33250a23a66e58370ed13b09bb39a0be2711c
Andres Gesteira <andres.gesteira@henkel.com>
2023-02-22 11:05:55 +0100

    comments on webpack extra config

--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -19,10 +19,41 @@ const shellWebpackConfig = {
       remotes: {
         mfes: 'mfes@http://localhost:4201/remoteEntry.js',
       },
+      /**
+       * All modules and libraries are singleton and shared by default.
+       * The `share` property in the Webpack configuration for the Module Federation plugin
+       * allows you to customize and control how the sharing behavior is applied,
+       * and specify any additional runtime dependencies that may be required.
+       *
+       * For example in this case we need to set these 2 packages to true to that the
+       * shell app loads them eagerly and not lazily when the component requetsts them.
+       * If that would happen you would get a null injector error for dependencies that must be
+       * initialized when an Angular app starts, that are containes in thsese pacakges.
+       *
+       * We will apply the same `share` configuration to the micfrofrontends.
+       */
       shared: share({
         '@angular/core': {
+          /**
+           * We we requiere same packages but different versions,
+           * Webpack automatically loads both if there are breaking changes between the 2.
+           * However, if you wanna risk it and still load only one of them,
+           * you can set this property to true.
+           * You will still get a warning in the console if a module requests an unsatisfied version.
+           */
           singleton: true,
+          /**
+           * But if you want those breaking changes to shout out at you
+           * (e.g. lib requieres version 1 but we are only loading version 2),
+           * you might be interested in getting an error instead of a warning
+           * by setting `strictVersion` to true.
+           */
           strictVersion: true,
+          /**
+           * This defines the version range.
+           * By default (auto) Webpack takes the
+           * highest version compatible with both modules.
+           */
           requiredVersion: 'auto',
           eager: true,
           includeSecondaries: {
