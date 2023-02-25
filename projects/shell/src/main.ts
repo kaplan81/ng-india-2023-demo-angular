52ce3646f8cb34e86d0cbd691eac9728157aa68c
Andres Gesteira <andres.gesteira@henkel.com>
2023-02-25 15:13:22 +0530

    remove comment

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,6 +1,5 @@
 import { loadRemoteEntry } from '@angular-architects/module-federation';
 
-// import('./bootstrap');
 loadRemoteEntry({ type: 'module', remoteEntry: 'http://localhost:4201/remoteEntry.js' }).then(() =>
   import('./bootstrap').catch((err) => console.error(err)),
 );
